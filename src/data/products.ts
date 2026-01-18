ce7f7bfe595cff57070946abc192d657d2233c77
Mukhtarrahimi <mukhtarrahimi.dev@gmail.com>
2026-01-18 14:08:01 +0430

    data/products.ts  -> Mock data

---
+++ src/data/products.ts
@@ -0,0 +1,23 @@
+export const products = [
+  {
+    id: "1",
+    title: "گوشی Samsung A54",
+    price: 310,
+    image: "/images/sample1.jpg",
+    location: "کابل، شهر نو",
+  },
+  {
+    id: "2",
+    title: "لباس ورزشی مردانه",
+    price: 40,
+    image: "/images/sample2.jpg",
+    location: "کابل، برچی ",
+  },
+  {
+    id: "3",
+    title: "لپ‌تاپ Dell Inspiron",
+    price: 480,
+    image: "/images/sample3.jpg",
+    location: "کابل، کوته سنگی ",
+  },
+];
